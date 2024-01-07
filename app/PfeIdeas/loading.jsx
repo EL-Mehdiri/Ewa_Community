c442e345a61ed3290d4fa46b3f1b709d7484bf3d
Mehdi Ridmani <117091960+EL-Mehdiri@users.noreply.github.com>
2024-01-07 11:46:14 +0100

    add skelaton to loading page

---
+++ app/PfeIdeas/loading.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Link from "next/link";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const LoadingIdeas = () => {
+  const ideas = [1, 2, 3, 4, 5, 6];
+  return (
+    <div className="">
+      <SkeletonTheme baseColor="#202020" highlightColor="#222666">
+        <button>
+          <Link href="/PfeIdeas/newIdea">
+            <Skeleton />
+          </Link>
+        </button>
+        <div className="grid grid-cols-3 gap-10 pt-5">
+          {ideas.map((idea) => (
+            <div
+              className="max-w-xl p-5 bg-gray-900 rounded-lg m-1 space-y-5 "
+              key={idea}
+            >
+              <h2 className="text-blue-400">
+                <Skeleton />
+              </h2>
+              <p>
+                <Skeleton count={5} />
+              </p>
+            </div>
+          ))}
+        </div>
+      </SkeletonTheme>
+    </div>
+  );
+};
+
+export default LoadingIdeas;
--- app/PfeIdeas/page.jsx
+++ app/PfeIdeas/page.jsx
@@ -1,11 +1,25 @@
+import prisma from "@/prisma/client";
 import Link from "next/link";
-
-const page = () => {
+import delay from "delay";
+const page = async () => {
+  const ideas = await prisma.pfeideas.findMany();
+  await delay(2000);
   return (
     <div className="">
       <button>
         <Link href="/PfeIdeas/newIdea">New Pfe Idea</Link>
       </button>
+      <div className="grid grid-cols-3 gap-10 pt-5">
+        {ideas.map((idea) => (
+          <div
+            className="max-w-xl p-5 bg-gray-900 rounded-lg m-1 space-y-5"
+            key={idea.id}
+          >
+            <h2 className="text-blue-400">{idea.title}</h2>
+            <p>{idea.content}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
