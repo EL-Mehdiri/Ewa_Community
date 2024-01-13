67613d6ff27ef2f223d2314a10ba2454fb220c77
Mehdi Ridmani <117091960+EL-Mehdiri@users.noreply.github.com>
2024-01-13 16:26:54 +0100

    .

--- app/PageAnimation.jsx
+++ app/PageAnimation.jsx
@@ -10,7 +10,7 @@ const PageWrapper = ({ children }) => {
           initial={{ opacity: 0, y: 15 }}
           animate={{ opacity: 1, y: 0 }}
           exist={{ opacity: 0, y: 15 }}
-          transition={{ delay: 0.25 }}
+          transition={{ delay: 0.35 }}
         >
           {children}
         </motion.div>
