753613717b289fbb5d97a7ace29f8f982471b5b3
Mehdi Ridmani <117091960+EL-Mehdiri@users.noreply.github.com>
2024-01-13 21:25:41 +0100

    cache

--- app/PfeIdeas/page.jsx
+++ app/PfeIdeas/page.jsx
@@ -3,15 +3,18 @@ import Content from "../components/mainPage/ContentPage";
 import Latest from "../components/mainPage/Latest";
 import Banner from "../components/mainPage/Banner";
 import PageWrapper from "../PageAnimation";
+import { cache } from "react";
+
+const fetchIdeas = cache(() => prisma?.pfeideas?.findMany());
 
 const page = async () => {
-  const ideas = await prisma?.pfeideas?.findMany();
+  const ideas = await fetchIdeas();
 
   return (
     <PageWrapper>
       <div className="grid gap-6 grid-cols-3">
         <div className="col-span-2 ">
-          <Banner href="/SharingLink/newLink" text={"Ideas"} />
+          <Banner href="/PfeIdea/newIdea" text={"Ideas"} />
           {ideas.map((idea) => (
             <Content key={idea.id} data={idea} href={`/PfeIdeas/${idea.id}`} />
           ))}
