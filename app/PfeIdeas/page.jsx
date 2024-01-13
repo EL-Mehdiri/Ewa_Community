68781d1f2a952b71e98d68a4ee2b5dfc552b485b
Mehdi Ridmani <117091960+EL-Mehdiri@users.noreply.github.com>
2024-01-13 16:07:09 +0100

    add framer motion

--- app/PfeIdeas/page.jsx
+++ app/PfeIdeas/page.jsx
@@ -2,22 +2,25 @@ import prisma from "@/prisma/client";
 import Content from "../components/mainPage/ContentPage";
 import Latest from "../components/mainPage/Latest";
 import Banner from "../components/mainPage/Banner";
+import PageWrapper from "../PageAnimation";
 
 const page = async () => {
   const ideas = await prisma?.pfeideas?.findMany();
 
   return (
-    <div className="grid gap-6 grid-cols-3">
-      <div className="col-span-2 ">
-        <Banner href="/SharingLink/newLink" />
-        {ideas.map((idea) => (
-          <Content key={idea.id} data={idea} href={`/PfeIdeas/${idea.id}`} />
-        ))}
+    <PageWrapper>
+      <div className="grid gap-6 grid-cols-3">
+        <div className="col-span-2 ">
+          <Banner href="/SharingLink/newLink" />
+          {ideas.map((idea) => (
+            <Content key={idea.id} data={idea} href={`/PfeIdeas/${idea.id}`} />
+          ))}
+        </div>
+        <div className="col-span-1">
+          <Latest />
+        </div>
       </div>
-      <div className="col-span-1">
-        <Latest />
-      </div>
-    </div>
+    </PageWrapper>
   );
 };
 
--- app/SharingLink/page.jsx
+++ app/SharingLink/page.jsx
@@ -3,22 +3,29 @@ import prisma from "@/prisma/client";
 import Content from "../components/mainPage/ContentPage";
 import Latest from "../components/mainPage/Latest";
 import Banner from "../components/mainPage/Banner";
+import PageWrapper from "../PageAnimation";
 
 const page = async () => {
   const links = await prisma?.sharingLinks?.findMany();
 
   return (
-    <div className="grid gap-6 grid-cols-3">
-      <div className="col-span-2 ">
-        <Banner href="/SharingLink/newLink" />
-        {links.map((link) => (
-          <Content key={link.id} data={link} href={`/SharingLink/${link.id}`} />
-        ))}
+    <PageWrapper>
+      <div className="grid gap-6 grid-cols-3">
+        <div className="col-span-2 ">
+          <Banner href="/SharingLink/newLink" />
+          {links.map((link) => (
+            <Content
+              key={link.id}
+              data={link}
+              href={`/SharingLink/${link.id}`}
+            />
+          ))}
+        </div>
+        <div className="col-span-1">
+          <Latest />
+        </div>
       </div>
-      <div className="col-span-1">
-        <Latest />
-      </div>
-    </div>
+    </PageWrapper>
   );
 };
 
